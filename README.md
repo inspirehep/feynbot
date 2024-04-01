f7c005c03e3d04f4c315d96c3c36b7ff15381950
Jaime Collado <jcolladom95@gmail.com>
2024-04-01 14:24:36 +0200

    Initial demo

---
+++ README.md
@@ -0,0 +1,6 @@
+This demo has been built following the official LlamaIndex [Starter Tutorial (Local Models)](https://docs.llamaindex.ai/en/stable/getting_started/starter_example_local/). It also implements index persistence from [Starter Tutorial (OpenAI)](https://docs.llamaindex.ai/en/stable/getting_started/starter_example/).
+
+Models tested:
+
+* Embedding model: [BAAI/bge-small-en-v1.5](https://huggingface.co/BAAI/bge-small-en-v1.5)
+* LLM: [Llama2](https://ollama.com/library/llama2) served through [Ollama](https://github.com/ollama/ollama)
---
+++ starter.py
@@ -0,0 +1,34 @@
+import os.path
+from llama_index.core import (
+    VectorStoreIndex,
+    SimpleDirectoryReader,
+    StorageContext,
+    Settings,
+    load_index_from_storage,
+)
+from llama_index.core.embeddings import resolve_embed_model
+from llama_index.llms.ollama import Ollama
+
+# bge embedding model
+Settings.embed_model = resolve_embed_model("local:BAAI/bge-small-en-v1.5")
+
+# ollama
+Settings.llm = Ollama(model="llama2", request_timeout=180.0)
+
+# check if storage already exists
+PERSIST_DIR = "./storage"
+if not os.path.exists(PERSIST_DIR):
+    # load the documents and create the index
+    documents = SimpleDirectoryReader("data").load_data()
+    index = VectorStoreIndex.from_documents(documents)
+    # store it for later
+    index.storage_context.persist(persist_dir=PERSIST_DIR)
+else:
+    # load the existing index
+    storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
+    index = load_index_from_storage(storage_context)
+
+# Either way we can now query the index
+query_engine = index.as_query_engine()
+response = query_engine.query("What did the author do growing up?")
+print(response)
