ce3b46031d99e01711faa5776fde10b0f8da6002
Miguel Garcia Garcia <miguel.garcia.garcia@cern.ch>
2025-02-19 14:15:44 +0100

    fix fulltext langchain search

--- ai-backend/src/ir_pipeline/chains.py
+++ ai-backend/src/ir_pipeline/chains.py
@@ -1,13 +1,14 @@
 from langchain_core.language_models import BaseLanguageModel
-from langchain_core.output_parsers import PydanticOutputParser, StrOutputParser
+from langchain_core.output_parsers import PydanticOutputParser
 from langchain_core.prompts import PromptTemplate
 
-from src.ir_pipeline.llm_response import LLMResponse
+from src.ir_pipeline.schemas import LLMResponse, Terms
 
 
 def create_query_expansion_chain(llm: BaseLanguageModel, system_prompt: str):
     prompt_template = PromptTemplate.from_template(system_prompt)
-    return prompt_template | llm | StrOutputParser()
+    output_parser = PydanticOutputParser(pydantic_object=Terms)
+    return prompt_template | llm | output_parser
 
 
 def create_answer_generation_chain(llm: BaseLanguageModel, system_prompt: str):
