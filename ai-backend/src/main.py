ca4b4824180c03d75c1c077e443509401d9deca9
Miguel Garcia Garcia <miguel.garcia.garcia@cern.ch>
2025-03-03 16:14:24 +0100

    backend: add fastapi metrics

--- ai-backend/src/main.py
+++ ai-backend/src/main.py
@@ -2,6 +2,7 @@ import logging
 from os import getenv
 
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 from .api import v1
 
@@ -10,3 +11,5 @@ logging.basicConfig(format="%(levelname)s - %(name)s:%(lineno)d - %(message)s")
 app = FastAPI(root_path=getenv("ROOT_PATH", ""))
 
 app.include_router(v1.router, prefix="/v1")
+
+Instrumentator().instrument(app).expose(app)
