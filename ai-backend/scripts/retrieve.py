11771079a4dcc8519e1c21a3766e288d51987b06
PascalEgn <pascal.egner@googlemail.com>
2025-06-24 13:51:46 +0200

    ai-backend: update embedding and retrieval script

--- ai-backend/scripts/retrieve.py
+++ ai-backend/scripts/retrieve.py
@@ -1,14 +1,82 @@
+import time
 from os import getenv
 
+from backend.src.ir_pipeline.utils.embeddings import VLLMOpenAIEmbeddings
+from backend.src.ir_pipeline.utils.reranker import CustomJinaRerank
+from backend.src.utils.langfuse import get_prompt
 from dotenv import load_dotenv
+from langchain_community.llms import VLLMOpenAI
 from langchain_community.vectorstores import OpenSearchVectorSearch
-from langchain_ollama import OllamaEmbeddings
 
 load_dotenv()
 
-embeddings = OllamaEmbeddings(model="nomic-embed-text")
+
+def pretty_print_docs(docs):
+    print(
+        f"\n{'-' * 100}\n".join(
+            [
+                f"Document {i + 1}:\n\n"
+                + str(d.metadata["control_number"])
+                + " "
+                + str(d.page_content)
+                for i, d in enumerate(docs)
+            ]
+        )
+    )
+
+
+def pretty_print_sources(docs):
+    print(
+        "\n".join(
+            [
+                f"Document {i + 1}: "
+                + "https://inspirehep.net/literature/"
+                + str(d.metadata["control_number"])
+                for i, d in enumerate(docs)
+            ]
+        )
+    )
+
+
+embeddings = VLLMOpenAIEmbeddings(
+    model_name=getenv("EMBEDDING_MODEL"),
+    openai_api_base=f"{getenv('API_BASE')}/v1",
+    openai_api_key=getenv("KUBEFLOW_API_KEY"),
+    default_headers=(
+        {"Host": getenv("KUBEFLOW_EMBEDDING_HOST")}
+        if getenv("KUBEFLOW_EMBEDDING_HOST")
+        else {}
+    ),
+    timeout=10,
+)
+
+reranker = CustomJinaRerank(
+    model_name=getenv("RERANKING_MODEL"),
+    openai_api_base=f"{getenv('API_BASE')}/v1",
+    openai_api_key=getenv("KUBEFLOW_API_KEY"),
+    default_headers=(
+        {"Host": getenv("KUBEFLOW_RERANKING_HOST")}
+        if getenv("KUBEFLOW_RERANKING_HOST")
+        else {}
+    ),
+    top_n=15,
+    timeout=40,
+)
+
+llm = VLLMOpenAI(
+    model_name=getenv("LLM_MODEL"),
+    openai_api_base=f"{getenv('API_BASE')}/v1",
+    default_headers=(
+        {"Host": getenv("KUBEFLOW_LLM_HOST")} if getenv("KUBEFLOW_LLM_HOST") else {}
+    ),
+    openai_api_key=getenv("KUBEFLOW_API_KEY"),
+    temperature=0,
+    top_p=1,
+    timeout=20,
+)
+
 vector_store = OpenSearchVectorSearch(
-    index_name="vector_demo_langchain",
+    index_name="embeddings_nucl-ex",
     embedding_function=embeddings,
     opensearch_url=getenv("VECTOR_DB_HOST"),
     http_auth=(
@@ -25,18 +93,48 @@ search_filter = [
     {
         "bool": {
             "should": [
-                {"term": {"metadata.embedding_type": "title"}},
+                {"term": {"metadata.embedding_type": "fulltext"}},
                 {"term": {"metadata.embedding_type": "abstract"}},
             ],
             "minimum_should_match": 1,
         }
     }
 ]
-docs = vector_store.similarity_search_with_score(
-    "What is a star?", k=100, filter=search_filter
+
+search_query = """
+Whats being discussed in the Collider constraints?
+"""
+
+prompt_template, _ = get_prompt("rag-query")
+
+start_retrieval = time.time()
+docs = vector_store.similarity_search(
+    query=search_query,
+    k=25,
 )
-for doc, score in docs:
-    print(f"Score: {score}")
-    print(f"Metadata: {doc.metadata}")
-    print("\n")
-print(len(docs))
+end_retrieval = time.time()
+
+start_reranking = time.time()
+reranked_docs = reranker.compress_documents(
+    documents=docs,
+    query=search_query,
+)
+end_reranking = time.time()
+
+message = prompt_template.invoke({"question": search_query, "context": reranked_docs})
+
+start_llm = time.time()
+response = llm.invoke(message)
+end_llm = time.time()
+
+print(response)
+
+
+print("The response is based on the following documents:")
+pretty_print_sources(reranked_docs)
+
+
+print("Retrieval time: ", end_retrieval - start_retrieval)
+print("Reranking time: ", end_reranking - start_reranking)
+print("LLM time: ", end_llm - start_llm)
+print("Total time: ", end_llm - start_retrieval)
--- ai-backend/scripts/utils.py
+++ ai-backend/scripts/utils.py
@@ -9,6 +9,11 @@ from opensearchpy import OpenSearch
 load_dotenv()
 
 
+def log_exception(idx, exception):
+    with open("error_logs.txt", "a") as log_file:
+        log_file.write(f"[{idx}] {exception}\n")
+
+
 def get_inspire_os_client():
     return OpenSearch(
         hosts=[
@@ -44,10 +49,8 @@ def get_vector_os_client(embedding_function, index_name="vector_demo_langchain")
     )
 
 
-def get_os_query(arXiv_category, full_text_available=True):
-    query = {
-        "bool": {"must": [{"term": {"arxiv_eprints.categories": f"{arXiv_category}"}}]}
-    }
+def get_os_query(full_text_available=True):
+    query = {"bool": {"must": []}}
     if full_text_available:
         query["bool"]["must"].append({"term": {"documents.source": "arxiv"}})
     return query
@@ -121,11 +124,11 @@ def process_hit(index, hit, vector_store, title_abstract_only, text_splitter):
             hit, title_abstract_only=title_abstract_only, text_splitter=text_splitter
         )
         vector_store.add_documents(langchain_documents)
-        print(f"[{index}] Successfully processed.")
         return True
     except Exception as e:
-        print(f"[{index}] Error processing record: {e}")
-        return False
+        control_number = get_record_metadata(hit)[0]
+        print(f"[{control_number}] Error processing record: {e}")
+        log_exception(control_number, e)
 
 
 def delete_control_number_from_index(vector_store, control_number):
@@ -147,3 +150,57 @@ def delete_control_number_from_index(vector_store, control_number):
         print(f"Deleted {len(ids)} records with control number {control_number}.")
     except Exception as e:
         print(f"Error deleting documents with control number {control_number}: {e}")
+
+
+def get_indexed_control_numbers(vector_store, output_file=None):
+    scroll_size = 10000
+    body = {
+        "size": 0,
+        "aggs": {
+            "unique_control_numbers": {
+                "composite": {
+                    "size": scroll_size,
+                    "sources": [
+                        {
+                            "control_number": {
+                                "terms": {"field": "metadata.control_number"}
+                            }
+                        }
+                    ],
+                }
+            }
+        },
+    }
+
+    after_key = None
+    total_collected = 0
+    all_control_numbers = []
+
+    if output_file:
+        f = open(output_file, "w")  # noqa E501
+
+    while True:
+        if after_key:
+            body["aggs"]["unique_control_numbers"]["composite"]["after"] = after_key
+
+        response = vector_store.client.search(index=vector_store.index_name, body=body)
+        agg = response["aggregations"]["unique_control_numbers"]
+        buckets = agg["buckets"]
+
+        if not buckets:
+            break
+
+        for bucket in buckets:
+            control_number = bucket["key"]["control_number"]
+            if output_file:
+                f.write(str(control_number) + "\n")
+            else:
+                all_control_numbers.append(control_number)
+            total_collected += 1
+
+        after_key = agg.get("after_key")
+
+    if output_file:
+        f.close()
+    else:
+        return all_control_numbers
